116ab4f24bc66add6b6c23d803beb1df5716ac99
MauricioZamperetti <mauriciohz2990@hotmail.com>
2022-05-17 17:00:03 -0300

    Finalizando o estudo de Teste de Exceção

--- TesteExcecao/Program.cs
+++ TesteExcecao/Program.cs
@@ -12,14 +12,19 @@ namespace TesteExcecao
         {
             try
             {
-                Metodo();
-
-                Console.ReadLine();
+                Metodo();                
             }
-            catch (NullReferenceException)
+            catch (DivideByZeroException)
             {
+                Console.WriteLine("Não é possível dividir por 0");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
                 Console.WriteLine("Aconteceu um erro.");
             }
+            Console.ReadLine();
         }
 
         //Teste com a cadeia de chama:
@@ -31,25 +36,22 @@ namespace TesteExcecao
         }
 
         private static void TestaDivisao(int divisor)
-        {
-            try
-            {
-                int resultado = Dividir(10, divisor);
-
-                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
-            }
-            catch (DivideByZeroException erro)
-            {
-                Console.WriteLine(erro.Message);
-                Console.WriteLine(erro.StackTrace);
-                Console.WriteLine("Não é possível dividir por zero.");
-            }
+        {            
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);     
         }
 
         private static int Dividir(int numero, int divisor)
         {
-            return numero / divisor;
-            //TESTESTETSETTESTSE
+            try
+            {
+                return numero / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Exceção com número=" + numero + " e divisor=" + divisor);
+                throw;              
+            }                    
         }
     }
 }
